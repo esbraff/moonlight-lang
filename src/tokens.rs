eca87bd1142b36b9468e9ab15e210154ca4ffaaf
esbraff <esbraff@yandex.ru>
2018-09-17 18:26:33 +0300

    Replacing tabs with spaces in the src/tokens.rs

--- src/tokens.rs
+++ src/tokens.rs
@@ -2,39 +2,39 @@
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub enum TokenType {
-	Number, // any number in decimal notation
-	HexNumber, // any number in hex notation
-	KeyWord,
-	StringValue,
+    Number, // any number in decimal notation
+    HexNumber, // any number in hex notation
+    KeyWord,
+    StringValue,
     Setter, // <-
 
-	Multiply, // *
-	Divide, // /
-	Add, // +
-	Substract, // -
+    Multiply, // *
+    Divide, // /
+    Add, // +
+    Substract, // -
 
-	LeftParent, // (
-	RightParent, // )
+    LeftParent, // (
+    RightParent, // )
 
-	EOF // end of file
+    EOF // end of file
 }
 
 impl ToString for TokenType {
     fn to_string(&self) -> String {
-    	match self {
-    	    TokenType::Number => "Number".to_string(),
-    	    TokenType::HexNumber => "HexNumber".to_string(),
-    	    TokenType::KeyWord => "KeyWord".to_string(),
+        match self {
+            TokenType::Number => "Number".to_string(),
+            TokenType::HexNumber => "HexNumber".to_string(),
+            TokenType::KeyWord => "KeyWord".to_string(),
             TokenType::StringValue => "StringValue".to_string(),
-    	    TokenType::Setter => "<-".to_string(),
-    	    TokenType::Multiply => "*".to_string(),
-    	    TokenType::Divide => "/".to_string(),
-    	    TokenType::Add => "+".to_string(),
-    	    TokenType::Substract => "-".to_string(),
-    	    TokenType::LeftParent => "(".to_string(),
-    	    TokenType::RightParent => ")".to_string(),
-    	    TokenType::EOF => "EOF".to_string()
-    	}
+            TokenType::Setter => "<-".to_string(),
+            TokenType::Multiply => "*".to_string(),
+            TokenType::Divide => "/".to_string(),
+            TokenType::Add => "+".to_string(),
+            TokenType::Substract => "-".to_string(),
+            TokenType::LeftParent => "(".to_string(),
+            TokenType::RightParent => ")".to_string(),
+            TokenType::EOF => "EOF".to_string()
+        }
     }
 }
 
@@ -46,10 +46,10 @@ pub struct Token {
 }
 
 impl Token {
-	pub fn new(token_type: TokenType, data: String) -> Token {
-		Token {
-			token_type: token_type,
-			data: data
-		}
-	}
+    pub fn new(token_type: TokenType, data: String) -> Token {
+        Token {
+            token_type: token_type,
+            data: data
+        }
+    }
 }
