8dde35fb3a57fa6a3dbaeeeac7b4dee0acf804f3
esbraff <esbraff@yandex.ru>
2018-09-18 16:54:28 +0300

    Example Addedr

---
+++ example/test.ml
@@ -0,0 +1,21 @@
+PI
+2
+0x01F
+\"string\"
+double_var <- 2 + 2 * 2
+string_var <- \"Hello, World!\"
+string_var ><
+double_var <- double_var + (0xAF / 1.5)
+double_var <- null
+
+print <- function(value) -> value
+
+mulBy2 <- function(value) {
+    multiplied <- value * 2
+
+    multiplied
+}
+
+mulBy2(11)
+
+print(\"Hello, World!\")
